53e966cdcb682bda5011f208ba76fe11397ba1d6
emanuelhenrique-dev <emanuelhenriquefs@gmail.com>
2022-10-12 15:54:10 -0300

    primeira parte

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './styles/global.css'
+
+export function App() {
+
+  return (
+    <>
+    <h1 className='text-violet-300 text-5xl'>Hello World</h1>
+
+    <button className='bg-violet-500 font-medium px-4 py-2 rounded text-white hover:bg-blue-800'>
+      Enviar
+    </button>
+    </>
+  )
+}
+
+
